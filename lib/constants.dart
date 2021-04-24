c25dbfc0175827ce2b82ac5052b3183bf5d06e29
Manoj Kumar Patra <patra.manoj0@gmail.com>
2021-04-24 11:39:00 +0530

    Set app constants

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const COLORS = {
+  'SECONDARY': Color(0xFFFE6D8E),
+  'TEXT': Color(0xFF12153D),
+  'TEXT_LIGHT': Color(0xFF9A9BB2),
+  'FILL_STAR': Color(0xFFFCC419),
+};
+
+const DEFAULT_PADDING = 20.0;
+
+const DEFAULT_SHADOW = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: Colors.black26,
+);
